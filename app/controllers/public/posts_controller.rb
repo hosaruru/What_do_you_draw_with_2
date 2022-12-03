46331437ac25c5da204ad92bb90277dfb647dd6d
EC2 Default User <ec2-user@ip-172-31-46-87.ec2.internal>
2022-12-03 18:50:28 +0000

    221204

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -41,7 +41,7 @@ class Public::PostsController < ApplicationController
   end
 
   def edit
-
+    @post = Post.find(params[:id])
   end
 
   def update
--- app/models/pen.rb
+++ app/models/pen.rb
@@ -1,4 +1,5 @@
 class Pen < ApplicationRecord
   belongs_to :post
+
   # validates :use_pen, presence: true
 end
