dc78437e77e71723d4cd2cfa8f3c692a5ca97517
hosaruru <sumera3232@gmail.com>
2023-01-21 17:03:28 +0000

    230122

--- spec/factories/board.rb
+++ spec/factories/board.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :board do
     headline { Faker::Lorem.characters(number:10) }
-    question { Faker::Lorem.characters(number:30) }
+    question { Faker::Lorem.characters(number:50) }
     user
   end
 end
