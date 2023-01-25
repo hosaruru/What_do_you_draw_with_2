f1d3ccdabddd50098c4edc183121cea24ec3f1c6
hosaruru <sumera3232@gmail.com>
2023-01-25 15:50:08 +0000

    230126

--- ruby/hello.rb
+++ ruby/hello.rb
@@ -1,16 +1,37 @@
-n = gets.to_i
+line = gets.chomp.split('')
+nyuryoku = gets.chomp
+kazu = line.length
 
-damage = {}
-n.times do
-  name = gets.chomp
-  damage[name] = 0
+kyoka = line.repeated_permutation(3).to_a
+kyoka_len = kyoka.length
+kyoka_list = []
+
+q = 0
+for i in 1..kyoka_len
+kyoka_list[q] = 
+
+
+kyoka[q][0]+kyoka[q][1]+kyoka[q][2]
+
+
+
+
+
+q += 1
+end
+ p kyoka_list
+line_len = line.length
+l = 0
+pass = ""
+for i in 1..line_len
+pass = pass + line[l]
+l += 1
 end
 
-m = gets.to_i
-m.times do
-  name, attack = gets.split(' ')
-  damage[name] += attack.to_i
+
+if kyoka_list.include?(nyuryoku) && !(nyuryoku == pass)
+  puts "YES"
+  else
+  puts "NO"   
 end
 
-s = gets.chomp
-puts damage[s]
