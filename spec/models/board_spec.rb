7ba6107169ed310d3340f123c04d660463870e85
hosaruru <sumera3232@gmail.com>
2023-02-02 14:47:21 +0000

    230202

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,14 +1,31 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
-RSpec.describe 'Postモデルのテスト', type: :model do
+RSpec.describe 'Boardモデルのテスト', type: :model do
+  describe 'バリデーションのテスト' do
+    subject { board.valid? }
+
     let(:user) { create(:user) }
-  describe '投稿のテスト' do
-    let!(:board) { create(:board,title:'hoge',body:'body') }
-    describe 'トップ画面(root_path)のテスト' do
-      before do 
-        visit root_path
+    let!(:board) { build(:board, user_id: user.id) }
+    
+    context 'headline' do
+      it '空欄でないこと' do
+        board.headline = ''
+        is_expected.to eq false
+      end
+    end
+
+    context 'question' do
+      it '空欄でないこと' do
+        board.question = ''
+        is_expected.to eq false
+      end
+    end
+  end
+
+  describe 'アソシエーションのテスト' do
+    context 'Userモデルとの関係' do
+      it 'N:1となっている' do
+        expect(Board.reflect_on_association(:user).macro).to eq :belongs_to
       end
     end
   end
