10fbf07e543e6adda71cf1cb3f9d27c319d4657f
hosaruru <sumera3232@gmail.com>
2023-02-13 13:01:15 +0000

    230213

--- app/controllers/public/board_comments_controller.rb
+++ app/controllers/public/board_comments_controller.rb
@@ -3,10 +3,11 @@ class Public::BoardCommentsController < ApplicationController
     @board = Board.find(params[:board_id])
     @board_comment = current_user.board_comments.new(board_comment_params)
     @board_comment.board_id = @board.id
-    if @board_comment.save
-      # @board_comment.create_notification_board_comment!     
-      @board_comment.create_notification_board_comment!
+    @board_comment.user_id = current_user.id
+    if @board_comment.save    
+      # @board_comment.create_notification_board_comment!
       @board_comment = BoardComment.new
+      @board_comment.create_notification_board_comment! 
       
     else
       render :error
